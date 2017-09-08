fa078a296e2aa0956c3f5593a2192e30da3f123b
Tyler K. Chafin <tkchafin@uark.edu>
2017-09-08 16:17:12 -0500

    functions for pw aln and sorting

--- vsearch.py
+++ vsearch.py
@@ -1,5 +1,53 @@
 #!/usr/bin/python
 
+from subprocess import Popen, PIPE, CalledProcessError
+import os
+
 """Includes utilities for calling VSEARCH and parsing output of pairwise alignments"""
 
-def vsearchALL(seqpath, cov, )
+def allpairsGlobal(binary, threads, seqpath, qid, qcov, outpath):
+    vsearch = [binary,
+            "--allpairs_global", seqpath,
+            "--threads", str(threads),
+            "--id", str(qid),
+            "--blast6out", ".temp.pw",
+            "--rowlen", "0", "--self",
+            "--target_cov", str(qcov),
+            "--quiet"]
+    command = " ".join(vsearch)
+
+    #Vsearch subprocess
+    print("Calling sub.Popen: ", command)
+    proc = Popen(vsearch, env={'PATH': os.getenv('PATH')})
+    print("Call is done ")
+    #WRap to enable keyboard interrupe
+    try:
+        t = proc.communicate()[0]
+    except KeyboardInterrupt:
+        proc.kill()
+        raise KeyboardInterrupt
+
+    #Get return code from process
+    if proc.returncode:
+        raise CalledProcessError ("VSEARCH exited with non-zero status")
+
+def sortByLength(binary, seqpath, outpath):
+    vsearch = [binary,
+            "--sortbylength", seqpath,
+            "--output", outpath]
+    command = " ".join(vsearch)
+
+    #Vsearch subprocess
+    print("Calling sub.Popen: ", command)
+    proc = Popen(vsearch, stdout=PIPE, stdin=PIPE, env={'PATH': os.getenv('PATH')})
+    print("Call is done ")
+    #WRap to enable keyboard interrupe
+    try:
+        t = proc.communicate()[0]
+    except KeyboardInterrupt:
+        proc.kill()
+        raise KeyboardInterrupt
+
+    #Get return code from process
+    if proc.returncode:
+        raise CalledProcessError ("VSEARCH exited with non-zero status")
