b8f9ca05c82b3c51baffcd73c42bbc805ce12e11
Tyler K. Chafin <tkchafin@uark.edu>
2017-09-10 14:18:29 -0500

    funcs to detect OS, path, and workdir

--- misc_utils.py
+++ misc_utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import sys
+import os
+
 #Function calculates union length of overlapping fixed length lines
 def calculateUnionLengthFixed(n, l, o):
     #N is number of lines
@@ -14,3 +17,24 @@ def calculateUnionLengthFixed(n, l, o):
 #Function to count number of lower case in a string
 def n_lower_chars(string):
     return sum(1 for c in string if c.islower())
+
+#Function to return what OS platform we are using
+def getOS():
+	platform = "unknown"
+	try:
+		platform = sys.platform
+	except OSError as e:
+            # try to catch windows-specific issues... MrBait not currently supported in Windows!!
+            if sys.platform.startswith('win'):
+                if isinstance(e, WindowsError) and e.winerror == 267:
+                    raise InvalidFile('uses Windows special name (%s)' % e)
+            raise
+	return(sys.platform)
+
+#Function to get directory of running python executable
+def getScriptPath():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+#Function to get call path
+def getWorkingDir():
+    return os.getcwd()
