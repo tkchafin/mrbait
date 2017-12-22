31b36ff0bbd58d70ead902b50219765b7d017f40
Tyler K. Chafin <tkchafin@uark.edu>
2017-12-22 16:41:51 -0600

    started gff_parser (currently empty)

---
+++ gff3_parser.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import misc_utils as utils
+import alignment_tools as aln
+
+#TODO: Decided I don't like any of the available GFF3 parsers so I'm going to
+#write my own.
+
+#function to read a GFF file
+#Generator function, yields individual elements
+def read_gff(g):
+	pass
