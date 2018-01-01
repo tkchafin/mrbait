7045440626cf2b37e81ef697ea291da99749cf36
Tyler K. Chafin <tkchafin@uark.edu>
2018-01-01 16:07:33 -0600

    drafted simple GFF3 parser

--- gff3_parser.py
+++ gff3_parser.py
@@ -18,6 +18,35 @@ def splitAttributes(a):
 #function to read a GFF file
 #Generator function, yields individual elements
 def read_gff(g):
-	fields = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
-	GFFRecord = namedtuple("GFFRecord", gffInfoFields)
-	pass
+	bad = 0 #tracker for if we have bad lines
+	gf = open(g)
+	try:
+		with gf as file_object:
+			for line in file_object:
+				if line.startswith("#"): continue
+				line = line.strip() #strip leading/trailing whitespace
+				if not line: #skip empty lines
+					continue
+				things = line.split("\t") #split lines
+				if len(things) != 9:
+					if bad == 0:
+						print("Warning: GFF file does not appear to be standard-compatible. See https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md")
+						bad = 1
+					elif bad == 1:
+						sys.exit("Fatal error: GFF file does not appear to be standard-compatible. See https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md")
+				#line = utils.removeURL(line) #Sanitize any URLs out
+				GFFTuple = {} #Empty dict
+				#Load up dict, and sanitize inputs 
+				GFFTuple["seqid"] = None if things[0] == "." else things[0]
+				GFFTuple["source"] = None if things[1] == "." else things[1]
+				GFFTuple["type"] =  None if things[2] == "." else things[2]
+				GFFTuple["start"] = None if things[3] == "." else int(things[3])
+				GFFTuple["end"] = None if things[4] == "." else int(things[4])
+				GFFTuple["score"] = None if things[5] == "." else float(things[5])
+				GFFTuple["strand"] = None if things[6] == "." else things[6]
+				GFFTuple["phase"] = None if things[7] == "." else things[7]
+				GFFTuple["attributes"] = None if things[8] == "." else splitAttributes(things[8])
+
+				yield(GFFTuple)
+	finally:
+		gf.close()
