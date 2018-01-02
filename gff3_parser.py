5b02d11f05eeb4f07693703ca4a6f890c363b757
Tyler K. Chafin <tkchafin@uark.edu>
2018-01-02 14:55:50 -0600

    convert all attributes to lower case

--- gff3_parser.py
+++ gff3_parser.py
@@ -11,7 +11,7 @@ def splitAttributes(a):
 		stuff = thing.split("=")
 		if len(stuff) != 2: continue #Eats error silently, YOLO
 		key = stuff[0].lower()
-		value = stuff[1]
+		value = stuff[1].lower()
 		ret[key] = value
 	return ret
 
@@ -35,18 +35,18 @@ def read_gff(g):
 					elif bad == 1:
 						sys.exit("Fatal error: GFF file does not appear to be standard-compatible. See https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md")
 				#line = utils.removeURL(line) #Sanitize any URLs out
-				GFFTuple = {} #Empty dict
+				GFFRecord = {} #Empty dict
 				#Load up dict, and sanitize inputs
-				GFFTuple["seqid"] = None if things[0] == "." else urllib.parse.unquote(things[0])
-				GFFTuple["source"] = None if things[1] == "." else urllib.parse.unquote(things[1])
-				GFFTuple["type"] =  None if things[2] == "." else urllib.parse.unquote(things[2])
-				GFFTuple["start"] = None if things[3] == "." else int(things[3])
-				GFFTuple["end"] = None if things[4] == "." else int(things[4])
-				GFFTuple["score"] = None if things[5] == "." else float(things[5])
-				GFFTuple["strand"] = None if things[6] == "." else urllib.parse.unquote(things[6])
-				GFFTuple["phase"] = None if things[7] == "." else urllib.parse.unquote(things[7])
-				GFFTuple["attributes"] = None if things[8] == "." else splitAttributes(urllib.parse.unquote(things[8]))
+				GFFRecord["seqid"] = None if things[0] == "." else urllib.parse.unquote(things[0])
+				GFFRecord["source"] = None if things[1] == "." else urllib.parse.unquote(things[1])
+				GFFRecord["type"] =  None if things[2] == "." else urllib.parse.unquote(things[2])
+				GFFRecord["start"] = None if things[3] == "." else int(things[3])
+				GFFRecord["end"] = None if things[4] == "." else int(things[4])
+				GFFRecord["score"] = None if things[5] == "." else float(things[5])
+				GFFRecord["strand"] = None if things[6] == "." else urllib.parse.unquote(things[6])
+				GFFRecord["phase"] = None if things[7] == "." else urllib.parse.unquote(things[7])
+				GFFRecord["attributes"] = None if things[8] == "." else splitAttributes(urllib.parse.unquote(things[8]))
 
-				yield(GFFTuple)
+				yield(GFFRecord)
 	finally:
 		gf.close()
