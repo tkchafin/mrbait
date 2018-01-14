a7e2b5255e42941234f87a10fd0b11a21d13d9a3
Tyler K. Chafin <tkchafin@uark.edu>
2018-01-14 15:20:38 -0600

    functions for reverse complementing

--- sequence_tools.py
+++ sequence_tools.py
@@ -1,6 +1,83 @@
 #!/usr/bin/python
 
 import re
+import sys
+from itertools import product
+
+#Function to split character to IUPAC codes, assuing diploidy
+def get_iupac_caseless(char):
+	lower = False
+	if char.islower():
+		lower = True
+		char = char.upper()
+	iupac = {
+		"A"	: ["A"],
+		"G"	: ["G"],
+		"C"	: ["C"],
+		"T"	: ["T"],
+		"N"	: ["A", "C", "G", "T"],
+		"-"	: ["A", "C", "G", "T", "-"],
+		"R"	: ["A","G"],
+		"Y"	: ["C","T"],
+		"S"	: ["G","C"],
+		"W"	: ["A","T"],
+		"K"	: ["G","T"],
+		"M"	: ["A","C"],
+		"B"	: ["C","G","T"],
+		"D"	: ["A","G","T"],
+		"H"	: ["A","C","T"],
+		"V"	: ["A","C","G"]
+	}
+	ret = iupac[char]
+	if lower:
+		ret = [c.lower() for c in ret]
+	return ret
+
+
+#Function to expand ambiguous sequences
+#Generator function
+def expandAmbiquousDNA(sequence):
+   for i in product(*[get_iupac_caseless(j) for j in sequence]):
+      yield("".join(i))
+
+#Function to return reverse complement of a nucleotide, while preserving case
+def get_revComp_caseless(char):
+	lower = False
+	if char.islower():
+		lower = True
+		char = char.upper()
+	d = {
+		"A"	: "T",
+		"G"	: "C",
+		"C"	: "G",
+		"T"	: "A",
+		"N"	: "N",
+		"-"	: "-",
+		"R"	: "Y",
+		"Y"	: "R",
+		"S"	: "S",
+		"W"	: "W",
+		"K"	: "M",
+		"M"	: "K",
+		"B"	: "V",
+		"D"	: "H",
+		"H"	: "D",
+		"V"	: "B"
+	}
+	ret = d[char]
+	if lower:
+		ret = ret.lower()
+	return ret
+
+
+#Function to reverse complement a sequence, with case preserved
+def reverseComplement(seq):
+	comp = []
+	for i in (get_revComp_caseless(j) for j in seq):
+		comp.append(i)
+	return("".join(comp[::-1]))
+
+
 
 #Function to simplify a sequence
 def simplifySeq(seq):
