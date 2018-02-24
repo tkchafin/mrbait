9d0401a7209ffe59e641356f671d806d53269630
Tyler K. Chafin <tkchafin@uark.edu>
2018-02-24 15:07:47 -0600

    another attempt setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ def requires():
 setup(
     name="mrbait",
     packages=['mrbait'],
-    version="1.0.2",
+    version="1.0.3",
     description="Universal design of target-enrichment capture probes from genomic data",
     author='Tyler K. Chafin',
     author_email='tkchafin@uark.edu',
     url='https://github.com/tkchafin/mrbait',
+	
     install_requires=requires(),
-	packages=find_packages(),
     include_package_data=True,
     entry_points={'console_scripts': ['mrbait = mrbait.mrbait:main']},
     zip_safe=False,
