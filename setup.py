c52cda57d079674758b74d12957f664bf18f49bb
Tyler K. Chafin <tkchafin@uark.edu>
2018-02-24 16:30:22 -0600

    conda build still broken

--- setup.py
+++ setup.py
@@ -2,22 +2,18 @@
 
 from setuptools import setup, find_packages
 
-def requires():
-    """ gets packages from requirements.txt """
-    with open('requirements.txt') as infile:
-        return infile.read().splitlines()
-
 setup(
     name="mrbait",
     install_requires=requires(),
-    version="1.0.4",
+    version="1.0.5",
     description="Universal design of target-enrichment capture probes from genomic data",
     author='Tyler K. Chafin',
     author_email='tkchafin@uark.edu',
     url='https://github.com/tkchafin/mrbait',
 	packages=['mrbait'],
     include_package_data=True,
-    entry_points={'console_scripts': ['mrbait = mrbait.mrbait:main']},
+    entry_points={'console_scripts':
+		['mrbait = mrbait.mrbait:main']},
     zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 3.6',
