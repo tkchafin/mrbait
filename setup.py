866aff8f2ae503aa4d3eeb2b147787138980784a
Tyler K. Chafin <tkchafin@uark.edu>
2018-02-24 13:23:13 -0600

    functioning setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
-import glob
-import re
 
 def requires():
     """ gets packages from requirements.txt """
     with open('requirements.txt') as infile:
         return infile.read().splitlines()
 
-## Auto-update version from git repo tags
-INITFILE = "ipyrad/__init__.py"
-CUR_VERSION = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                    open(INITFILE, "r").read(),
-                    re.M).group(1)
-
 setup(
     name="mrbait",
-    version=CUR_VERSION,
+    version="1.0.1",
     url="https://github.com/tkchafin/mrbait",
     author="Tyler K. Chafin",
     author_email="tkchafin@uark.edu",
-    description="Universal design of target-enrichment capture probes from genomic data",
+    description="Interactive assembly and analysis of RADseq data sets",
     packages=find_packages(),
     install_requires=requires(),
-    #dependencies=dependency_links(),
     entry_points={
             'console_scripts': [
-                'ipyrad = mrbait.mrbait:main',
+                'mrbait = mrbait.mrbait:main'
             ],
     },
     license='GPL',
