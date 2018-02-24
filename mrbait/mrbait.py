67c6d429f750eb0b723beef66c5650018aef38cc
Tyler K. Chafin <tkchafin@uark.edu>
2018-02-24 15:18:42 -0600

    testing

--- mrbait/mrbait.py
+++ mrbait/mrbait.py
@@ -4,7 +4,7 @@ import sqlite3
 import pandas
 import time
 from timeit import default_timer as timer
-from mrbait import parseArgs
+from mrbait.mrbait_menu import ParseArgs
 import manage_bait_db as m
 import mrbait_corefuncs as core
 import mrbait_corefuncs_parallel as pcore
